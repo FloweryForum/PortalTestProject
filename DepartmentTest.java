1229752bf3c2037f18201448a06e63791a78c329
frathod <forumneosoftqa@gmail.com>
2022-12-02 02:57:31 +0530

    DeparmentTest.java file added

---
+++ DepartmentTest.java
@@ -0,0 +1,10 @@
+public class DepartmentTest{
+    private int deptId;
+    private String deptName;
+
+    public void display(){
+        System.out.println("Dept ID: "+deptId);
+        System.out.println("Dept Name: "+deptName);
+
+    }
+}
